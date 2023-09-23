2aecd99ff50a82995031ee98e7b3443cb3e56445
alirezaxdehghan <alirezaxdehghan@gmail.com>
2023-09-23 18:24:21 +0200

    Full implementation

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from random import choice
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class mazeCell:
+    def __init__(self, position):
+        self.position = position
+        self.next = None
+        self.direction = None
+        self.visited = False
+
+def generateMaze(size):
+    maze = []
+    mazeUnvisited = []
+    if size%2 == 0: size += 1 #size must be odd
+    for i in range(size):
+        maze.append([]) #the maze is a nxn nested set
+        for j in range(size):
+            newCell = mazeCell((i, j))
+            maze[i].append(newCell)
+            mazeUnvisited.append(newCell) #list of unvisited maze cells
+
+    VisitedCell = choice(choice(maze))
+    VisitedCell.visited = True      #marking a random cell of the maze as visited
+    mazeUnvisited.remove(VisitedCell)
+
+    while (mazeUnvisited):
+        CurrentCell = choice(mazeUnvisited)
+        FirstCell = CurrentCell
+        while CurrentCell.visited is False:
+            xy = CurrentCell.position
+            dir = choice([(0, 1), (0, -1), (1, 0), (-1, 0)]) #randomly choose next adjacent cell
+            while not (tuple(map(sum, zip(xy, dir))) >= (0, 0) and tuple(map(sum, zip(xy, dir)))[0] < len(maze) and
+                       tuple(map(sum, zip(xy, dir)))[1] < len(maze)): #check bounds real dirty
+                dir = choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+            i, j = tuple(map(sum, zip(xy, dir)))
+            CurrentCell.next = (i, j)
+            CurrentCell.direction = dir
+            CurrentCell = maze[i][j]
+
+        CurrentCell = FirstCell
+        while CurrentCell.visited is False:
+            CurrentCell.visited = True
+            mazeUnvisited.remove(CurrentCell)
+            CurrentCell = maze[CurrentCell.next[0]][CurrentCell.next[1]]
+    return maze
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    maze = generateMaze(8)
+    for cells in maze:
+        print("\n")
+        for cell in cells:
+            print(f"{cell.direction}",end=" | ")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
